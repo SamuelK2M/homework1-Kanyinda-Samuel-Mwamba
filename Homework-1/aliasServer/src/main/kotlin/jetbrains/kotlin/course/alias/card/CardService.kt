a2a3597e172210a4d70cc5f762ddc6f9d0415bf1
SK2ML <skanyinda78@gmail.com>
2025-04-01 23:52:38 +0200

    Assignment HW-1

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,12 +1,46 @@
 package jetbrains.kotlin.course.alias.card
 
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
+import jetbrains.kotlin.course.alias.util.words
 import org.springframework.stereotype.Service
 
 @Service
 class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+    private val identifierFactory: IdentifierFactory = IdentifierFactory()
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+
+    private fun generateCards(): List<Card> {
+        val shuffledWords = words.shuffled()
+        return shuffledWords
+            .chunked(wordsPerCard)
+            .take(cardsAmount)
+            .map { wordsChunk ->
+                Card(identifierFactory.uniqueIdentifier(), wordsChunk.toWords())
+            }
+    }
+
+    private fun List<String>.toWords(): List<Word> {
+        return this.map { string ->
+            Word().apply {
+                wordItem = string
+            }
+        }
+    }
+
+    private val cards: List<Card> = generateCards()
+
+    companion object {
+        private const val wordsPerCard = 3
+        var cardsAmount = words.size / wordsPerCard
+    }
+
+    fun getCardByIndex(index: Int): Card {
+        if (index < 0 || index >= cards.size) {
+            throw IllegalArgumentException("Card $index does not exist !")
+        }
+        return cards[index]
+    }
+
+
 }
--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
@@ -1,10 +1,32 @@
 package jetbrains.kotlin.course.alias.results
 
+import jetbrains.kotlin.course.alias.team.Team
+import jetbrains.kotlin.course.alias.team.TeamService
 import org.springframework.stereotype.Service
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
+    fun saveGameResults(result: GameResult): Unit {
+        if (result.isEmpty()){
+            throw IllegalArgumentException("No Results Found!")
+        }
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+        val allIdsExist = result.all { team -> TeamService.teamStorage.containsKey(team.id) }
+
+        if (!allIdsExist){
+            throw IndexOutOfBoundsException("Wrong Teams IDs")
+        }
+
+        gameHistory.add(result)
+    }
+
+    fun getAllGameResults(): List<GameResult>{
+        return gameHistory.asReversed()
+    }
+
+    companion object {
+        var gameHistory: MutableList<GameResult> = mutableListOf()
+    }
 }
